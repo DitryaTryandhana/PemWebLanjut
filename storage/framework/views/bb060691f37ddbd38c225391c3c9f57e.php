d53422bb0e4be71ffd5aba4a3411fc4b96c04237
DitryaTryandhana <149044989+DitryaTryandhana@users.noreply.github.com>
2025-03-18 19:55:30 +0700

    Add files via upload

---
+++ storage/framework/views/bb060691f37ddbd38c225391c3c9f57e.php
@@ -0,0 +1,9 @@
+<?php echo $__env->make('layouts.header', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+
+<?php echo $__env->make('layouts.navbar', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+
+<?php echo $__env->make('layouts.sidebar', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+
+<?php echo $__env->make('layouts.content', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+
+<?php echo $__env->make('layouts.footer', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\laragon\www\TugasPemWeb1\resources\views/layouts/master.blade.php ENDPATH**/ ?>
